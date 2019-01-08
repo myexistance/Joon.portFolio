0b658c36db7cc968ed90584e10c3daf7b6e94c02
myexistance <joonyoonlives@gmail.com>
2019-01-08 17:29:19 +1300

    typewriter added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
+// $(document).ready(function(){
+// 	$(".scroll").click(function(event){
+// 		event.preventDefault();
+// 		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+// 		$('.navbar-default a').removeClass('selected');
+// 		$(this).addClass('selected');
+//     	});
+// });
+
 $(document).ready(function(){
-	$(".scroll").click(function(event){
-		event.preventDefault();
-		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
-		$('.navbar-default a').removeClass('selected');
-		$(this).addClass('selected');
-    	});
-});
+ $("#home").click(function (){
+              $('html, body').animate({
+                  scrollTop: $(".about").offset().top-50
+              }, 800);
+          });
+ $("#about").click(function (){
+              $('html, body').animate({
+                  scrollTop: $(".food").offset().top-50
+              }, 800);
+          });
+ $("#portfolio").click(function (){
+              $('html, body').animate({
+                  scrollTop: $(".beer").offset().top-55
+              }, 800);
+          });
+ $("#contact").click(function (){
+              $('html, body').animate({
+                  scrollTop: $(".information").offset().top-55
+              }, 800);
+          });
