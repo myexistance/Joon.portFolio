d9fff233d6ed9a7cd49d98d01e8fff66a4eed5de
myexistance <joonyoonlives@gmail.com>
2019-01-28 19:28:23 +1300

    commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-// $(document).ready(function(){
-// 	$(".scroll").click(function(event){
-// 		event.preventDefault();
-// 		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
-// 		$('.navbar-default a').removeClass('selected');
-// 		$(this).addClass('selected');
-//     	});
+$(document).ready(function(){
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
+    	});
+});
+
+
+// show/hide the menu when examples is clicked
+// $(".dropdown-toggle").on("click", function () {
+//   $(".dropdown-menu").toggle();
 // });
+
+// hide the menu when an exmple is clicked
+// $("#home").on("click", function(){
+//     $(".navbar-nav").hide();
+// });
+// $("#about").on("click", function(){
+//     $(".navbar-nav").hide();
+// });
+// $("#portfolio").on("click", function(){
+//     $(".navbar-nav").hide();
+// });
+// $("#contact").on("click", function(){
+//     $(".navbar-nav").hide();
+// });
+
+
+
 
 $(document).ready(function(){
  $("#home").click(function (){
               $('html, body').animate({
-                  scrollTop: $(".about").offset().top-50
-              }, 90);
+                  scrollTop: $(".home").offset().top-50
+              }, 10);
           });
  $("#about").click(function (){
               $('html, body').animate({
-                  scrollTop: $(".food").offset().top-50
-              }, 90);
+                  scrollTop: $(".about").offset().top-50
+              }, 10);
           });
  $("#portfolio").click(function (){
               $('html, body').animate({
-                  scrollTop: $(".beer").offset().top-55
-              }, 90);
+                  scrollTop: $(".portfolio").offset().top-55
+              }, 10);
           });
  $("#contact").click(function (){
               $('html, body').animate({
-                  scrollTop: $(".information").offset().top-55
-              }, 90);
+                  scrollTop: $(".contact").offset().top-55
+              }, 10);
           });
