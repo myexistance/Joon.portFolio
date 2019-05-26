be9f17257d0ead6f01f393a3717f84c7d35d598e
myexistance <joonyoonlives@gmail.com>
2019-05-26 18:29:14 +1200

    wordcloud header replaced with Hello my name is Joon

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	$(".scroll").click(function(event){
 		event.preventDefault();
-		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 700);
 		$('.navbar-default a').removeClass('selected');
 		$(this).addClass('selected');
     	});
@@ -18,48 +18,48 @@ $(document).ready(function () {
   });
 });
 
-//word clouds
-$(document).ready(function() {
+// //word clouds
+// $(document).ready(function() {
 
-  var
-    words = $('#tagi text'),
-    l = words.length,
-    current = null,
-    delay = 2000;
+//   var
+//     words = $('#tagi text'),
+//     l = words.length,
+//     current = null,
+//     delay = 2000;
 
-  function clearBlink(o) {
-    var
-      ca = o.getAttribute('class').split(' '),
-      i = ca.indexOf('blink');
+//   function clearBlink(o) {
+//     var
+//       ca = o.getAttribute('class').split(' '),
+//       i = ca.indexOf('blink');
 
-    if (i !== -1) {
-      ca.splice(i, 1);
-      o.setAttribute('class', ca.join(' '));
-    }
-  }
+//     if (i !== -1) {
+//       ca.splice(i, 1);
+//       o.setAttribute('class', ca.join(' '));
+//     }
+//   }
 
-  function addBlink(o) {
-    var
-      ca = o.getAttribute('class').split(' ');
-    ca.push('blink');
-    o.setAttribute('class', ca.join(' '));
-  }
+//   function addBlink(o) {
+//     var
+//       ca = o.getAttribute('class').split(' ');
+//     ca.push('blink');
+//     o.setAttribute('class', ca.join(' '));
+//   }
 
-  function wordblink() {
+//   function wordblink() {
 
-    var e;
+//     var e;
 
-    if (current !== null) {
-      clearBlink(words.eq(current)[0])
-    }
+//     if (current !== null) {
+//       clearBlink(words.eq(current)[0])
+//     }
 
-    current = Math.floor(Math.random() * l);
-    e = words.eq(current);
-    addBlink(e[0]);
+//     current = Math.floor(Math.random() * l);
+//     e = words.eq(current);
+//     addBlink(e[0]);
 
-    setTimeout(wordblink, delay);
-  }
+//     setTimeout(wordblink, delay);
+//   }
 
-  wordblink();
+//   wordblink();
 
-});
+// });
